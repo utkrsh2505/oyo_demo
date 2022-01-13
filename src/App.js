a160964067d39aa2eb3e3c0373a3846a4517032c
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2022-01-13 21:26:03 +0530

    footer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./Components/footer/Footer";
+import { ToolTip } from './Components/Tooltip';
+function App() {
+  return (
+    <div className="App">
+      <ToolTip/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
